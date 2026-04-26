077d8ee687c7db99110c7f017dc8679e7ce448a0
victor ejike <victorejiki@gmail.com>
2026-04-26 22:46:05 +0100

    function that handle fixartic

---
+++ fixarticle.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func FixArticle(text string) string {
+	words := strings.Fields(text)
+
+	for i := 0; i < len(words)-1; i++ {
+		if words[i] == "a" && strings.Contains("aeiouh", strings.ToLower(string(words[i+1][0]))) {
+			words[i] = "an"
+		}
+	}
+	return strings.Join(words, " ")
+}
+
+func main() {
+	input := " a apple  a banana  a insert"
+	output := FixArticle(input)
+
+	fmt.Println("input", input)
+	fmt.Println("output", output)
+}
--- for.go
+++ for.go
@@ -1,29 +1,29 @@
+// // package main
+
+// // import (
+// // 	"fmt"
+// // )
+
+// // func main() {
+// // 	fruite := [3]string{"apple", "mango", "orange"}
+
+// //		for i, j := range fruite {
+// //			fmt.Printf("%v \t %v\n", i, j)
+// //		}
+// //	}
 // package main
 
 // import (
 // 	"fmt"
 // )
 
+// func MyFunction(fname string, age int) {
+// 	fmt.Println("hello,", fname, "welcomeback!", "and do you know your ", age)
+// }
+
 // func main() {
-// 	fruite := [3]string{"apple", "mango", "orange"}
-
-//		for i, j := range fruite {
-//			fmt.Printf("%v \t %v\n", i, j)
-//		}
-//	}
-package main
-
-import (
-	"fmt"
-)
-
-func MyFunction(fname string, age int) {
-	fmt.Println("hello,", fname, "welcomeback!", "and do you know your ", age)
-}
-
-func main() {
-	MyFunction("john", 18)
-	MyFunction("victor", 20)
-	MyFunction("ejike", 23)
-	MyFunction("nmesomma", 12)
-}
+// 	MyFunction("john", 18)
+// 	MyFunction("victor", 20)
+// 	MyFunction("ejike", 23)
+// 	MyFunction("nmesomma", 12)
+// }
